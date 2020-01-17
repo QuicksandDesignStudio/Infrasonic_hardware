d8bb771ab1dcd7e7d5d732abb993ad137014fd8d
QuicksandDesignStudio <romit@quicksand.co.in>
2020-01-17 16:00:32 +0530

    Adding gitignore

--- infrasonic/infrasonic.ino
+++ infrasonic/infrasonic.ino
@@ -14,7 +14,7 @@
 
   Copyright (c) 2015 Hristo Gochkov - Example
   Copyright (c) 2020 Thejesh GN - Added sampling, Intial Sequence, File Write, LED on and Off
-  Copyright (c) 2020 Romit
+  Copyright (c) 2020 Romit Raj
   
 
 */
@@ -35,53 +35,42 @@
 #include <SPIFFS.h>
 #endif
 
-
 #define DBG_OUTPUT_PORT Serial
-#define LED_BUILTIN 2  
+#define LED_BUILTIN 2
 
-const char* ssid = "wifi-ssid";
-const char* password = "wifi-password";
-const char* host = "esp32fs";
+const char *ssid = "wifi-ssid";
+const char *password = "wifi-password";
+const char *host = "esp32fs";
 WebServer server(80);
-
-
 
 long randNumber = 5000;
 
+void setup(void)
+{
 
-
-void setup(void) {
-
-   
-  
   //INIT ONBOAD LED and START THE INTIAL SEQUENCE
   pinMode(LED_BUILTIN, OUTPUT);
   ledDanceSequence();
-  
+
   DBG_OUTPUT_PORT.begin(115200);
   DBG_OUTPUT_PORT.print("\n");
   DBG_OUTPUT_PORT.setDebugOutput(true);
 
-
-
-
-  
-  if (FORMAT_FILESYSTEM) FILESYSTEM.format();
+  if (FORMAT_FILESYSTEM)
+    FILESYSTEM.format();
   FILESYSTEM.begin();
   {
-      File root = FILESYSTEM.open("/");
-      File file = root.openNextFile();
-      while(file){
-          String fileName = file.name();
-          size_t fileSize = file.size();
-          DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
-          file = root.openNextFile();
-      }
-      DBG_OUTPUT_PORT.printf("\n");
+    File root = FILESYSTEM.open("/");
+    File file = root.openNextFile();
+    while (file)
+    {
+      String fileName = file.name();
+      size_t fileSize = file.size();
+      DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+      file = root.openNextFile();
+    }
+    DBG_OUTPUT_PORT.printf("\n");
   }
-
-
-
 
   //SETUP WIFI AP
   DBG_OUTPUT_PORT.printf("Staring the Wifi AP to %s\n", ssid);
@@ -90,30 +79,25 @@ void setup(void) {
   Serial.print("AP IP address: ");
   Serial.println(myIP);
 
-
-  //START HTTP SERVER  
+  //START HTTP SERVER
   //server.begin();
   Serial.println("Server started");
 
-
   //Default will be a list of xmls
   server.on("/list", HTTP_GET, handleFileList);
-        
+
   //delete file
   server.on("/edit", HTTP_DELETE, handleFileDelete);
-  
-  
 
   //called when the url is not defined here
   //use it to load content from FILESYSTEM
   server.onNotFound([]() {
-    if (!handleFileRead(server.uri())) {
+    if (!handleFileRead(server.uri()))
+    {
       server.send(404, "text/plain", "FileNotFound");
     }
   });
 
-  
-  
   //THIS IS FOR DEBUG
   server.on("/debug", HTTP_GET, []() {
     String json = "{";
@@ -126,10 +110,10 @@ void setup(void) {
   });
   server.begin();
   DBG_OUTPUT_PORT.println("HTTP server started");
-
 }
 
-void loop(void) {
+void loop(void)
+{
 
   //SAMPLE SOUND EVERY FIVE MINUTES. LATER WE CAN ADD INTERRUPT BASED
   //const unsigned long fiveMinutes = 5 * 60 * 1000UL;
@@ -140,9 +124,7 @@ void loop(void) {
   {
     lastSampleTime += fiveMinutes;
     sampleSound();
-  }  
-
-
+  }
 
   //HANDLE HTTP REQUESTS
   server.handleClient();
@@ -153,23 +135,22 @@ void loop(void) {
  * This will sample the anologRead for given amount of time and then will write it to a file
  * 
  */
- void sampleSound(){
+void sampleSound()
+{
   DBG_OUTPUT_PORT.println("start sample sound");
   ledOn();
 
-  
   String filePre = "sample_";
   String filePost = ".xml";
-  
+
   //SETUP RANDOM READ FROM A ANALOG PIN
-  //randomSeed(analogRead(A0));  
+  //randomSeed(analogRead(A0));
   //randNumber = random(5000, 4294967295);
-  
+
   randNumber = randNumber + 1;
   String fileName = filePre + randNumber + filePost;
   DBG_OUTPUT_PORT.println("file name:");
   DBG_OUTPUT_PORT.println(fileName);
-  
 
   String csv_data = "124,123,364,387,938";
 
@@ -178,84 +159,102 @@ void loop(void) {
   XML_DATA.concat(csv_data);
   XML_DATA.concat("</sample>");
   XML_DATA.concat("</data>");
-  
-  createFile(fileName, XML_DATA);  
+
+  createFile(fileName, XML_DATA);
   ledOff();
   DBG_OUTPUT_PORT.println("end sample sound");
- }
-
+}
 
 /*###################################################### ON BOARD LED HANDLE ############################################################*/
 
-
-void ledDanceSequence(){  
-  for(int i =0; i < 10; i++){
-    if(i%2 == 0){
-      digitalWrite(LED_BUILTIN, HIGH); 
-    }else{
-      digitalWrite(LED_BUILTIN, LOW); 
-    }    
+void ledDanceSequence()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    if (i % 2 == 0)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
     delay(500);
   }
 }
 
-void ledOn(){  
-    digitalWrite(LED_BUILTIN, HIGH); 
+void ledOn()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
-void ledOff(){  
-    digitalWrite(LED_BUILTIN, LOW); 
+void ledOff()
+{
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 /*###################################################### FILE FUNCTIONS ############################################################*/
 
 //format bytes
-void createFile(String path, String data) {
-  if (exists(path)) {
-    DBG_OUTPUT_PORT.println("File already exists");    
+void createFile(String path, String data)
+{
+  if (exists(path))
+  {
+    DBG_OUTPUT_PORT.println("File already exists");
     File file = FILESYSTEM.open(path, "r");
-    DBG_OUTPUT_PORT.println(file);    
-    while (file.available()) { 
+    DBG_OUTPUT_PORT.println(file);
+    while (file.available())
+    {
       DBG_OUTPUT_PORT.println("file is availabe");
-       char c = file.read();
-       DBG_OUTPUT_PORT.println(c);
+      char c = file.read();
+      DBG_OUTPUT_PORT.println(c);
     }
     DBG_OUTPUT_PORT.println("close file");
     file.close();
-    
-  }else{
+  }
+  else
+  {
     DBG_OUTPUT_PORT.println("open for writing");
     File file = FILESYSTEM.open(path, "w");
-    if (file) {
+    if (file)
+    {
       file.print(data);
       file.close();
       DBG_OUTPUT_PORT.println("Wrote the data to file");
-    } else {
+    }
+    else
+    {
       DBG_OUTPUT_PORT.println("Couldn't write the file");
     }
-    
   }
 }
 
-
-
-String formatBytes(size_t bytes) {
-  if (bytes < 1024) {
+String formatBytes(size_t bytes)
+{
+  if (bytes < 1024)
+  {
     return String(bytes) + "B";
-  } else if (bytes < (1024 * 1024)) {
+  }
+  else if (bytes < (1024 * 1024))
+  {
     return String(bytes / 1024.0) + "KB";
-  } else if (bytes < (1024 * 1024 * 1024)) {
+  }
+  else if (bytes < (1024 * 1024 * 1024))
+  {
     return String(bytes / 1024.0 / 1024.0) + "MB";
-  } else {
+  }
+  else
+  {
     return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
   }
 }
 
-
-bool exists(String path){
+bool exists(String path)
+{
   bool yes = false;
   File file = FILESYSTEM.open(path, "r");
-  if(!file.isDirectory()){
+  if (!file.isDirectory())
+  {
     yes = true;
   }
   file.close();
@@ -264,47 +263,76 @@ bool exists(String path){
 
 /*###################################################### HANDLE SERVER FUNCTIONS ############################################################*/
 
-String getContentType(String filename) {
-  if (server.hasArg("download")) {
+String getContentType(String filename)
+{
+  if (server.hasArg("download"))
+  {
     return "application/octet-stream";
-  } else if (filename.endsWith(".htm")) {
+  }
+  else if (filename.endsWith(".htm"))
+  {
     return "text/html";
-  } else if (filename.endsWith(".html")) {
+  }
+  else if (filename.endsWith(".html"))
+  {
     return "text/html";
-  } else if (filename.endsWith(".css")) {
+  }
+  else if (filename.endsWith(".css"))
+  {
     return "text/css";
-  } else if (filename.endsWith(".js")) {
+  }
+  else if (filename.endsWith(".js"))
+  {
     return "application/javascript";
-  } else if (filename.endsWith(".png")) {
+  }
+  else if (filename.endsWith(".png"))
+  {
     return "image/png";
-  } else if (filename.endsWith(".gif")) {
+  }
+  else if (filename.endsWith(".gif"))
+  {
     return "image/gif";
-  } else if (filename.endsWith(".jpg")) {
+  }
+  else if (filename.endsWith(".jpg"))
+  {
     return "image/jpeg";
-  } else if (filename.endsWith(".ico")) {
+  }
+  else if (filename.endsWith(".ico"))
+  {
     return "image/x-icon";
-  } else if (filename.endsWith(".xml")) {
+  }
+  else if (filename.endsWith(".xml"))
+  {
     return "text/xml";
-  } else if (filename.endsWith(".pdf")) {
+  }
+  else if (filename.endsWith(".pdf"))
+  {
     return "application/x-pdf";
-  } else if (filename.endsWith(".zip")) {
+  }
+  else if (filename.endsWith(".zip"))
+  {
     return "application/x-zip";
-  } else if (filename.endsWith(".gz")) {
+  }
+  else if (filename.endsWith(".gz"))
+  {
     return "application/x-gzip";
   }
   return "text/plain";
 }
 
-
-bool handleFileRead(String path) {
+bool handleFileRead(String path)
+{
   DBG_OUTPUT_PORT.println("handleFileRead: " + path);
-  if (path.endsWith("/")) {
+  if (path.endsWith("/"))
+  {
     path += "index.htm";
   }
   String contentType = getContentType(path);
   String pathWithGz = path + ".gz";
-  if (exists(pathWithGz) || exists(path)) {
-    if (exists(pathWithGz)) {
+  if (exists(pathWithGz) || exists(path))
+  {
+    if (exists(pathWithGz))
+    {
       path += ".gz";
     }
     File file = FILESYSTEM.open(path, "r");
@@ -315,17 +343,20 @@ bool handleFileRead(String path) {
   return false;
 }
 
-
-void handleFileDelete() {
-  if (server.args() == 0) {
+void handleFileDelete()
+{
+  if (server.args() == 0)
+  {
     return server.send(500, "text/plain", "BAD ARGS");
   }
   String path = server.arg(0);
   DBG_OUTPUT_PORT.println("handleFileDelete: " + path);
-  if (path == "/") {
+  if (path == "/")
+  {
     return server.send(500, "text/plain", "BAD PATH");
   }
-  if (!exists(path)) {
+  if (!exists(path))
+  {
     return server.send(404, "text/plain", "FileNotFound");
   }
   FILESYSTEM.remove(path);
@@ -333,9 +364,10 @@ void handleFileDelete() {
   path = String();
 }
 
-
-void handleFileList() {
-  if (!server.hasArg("dir")) {
+void handleFileList()
+{
+  if (!server.hasArg("dir"))
+  {
     server.send(500, "text/plain", "BAD ARGS");
     return;
   }
@@ -343,24 +375,26 @@ void handleFileList() {
   String path = server.arg("dir");
   DBG_OUTPUT_PORT.println("handleFileList: " + path);
 
-
   File root = FILESYSTEM.open(path);
   path = String();
 
   String output = "[";
-  if(root.isDirectory()){
-      File file = root.openNextFile();
-      while(file){
-          if (output != "[") {
-            output += ',';
-          }
-          output += "{\"type\":\"";
-          output += (file.isDirectory()) ? "dir" : "file";
-          output += "\",\"name\":\"";
-          output += String(file.name()).substring(1);
-          output += "\"}";
-          file = root.openNextFile();
+  if (root.isDirectory())
+  {
+    File file = root.openNextFile();
+    while (file)
+    {
+      if (output != "[")
+      {
+        output += ',';
       }
+      output += "{\"type\":\"";
+      output += (file.isDirectory()) ? "dir" : "file";
+      output += "\",\"name\":\"";
+      output += String(file.name()).substring(1);
+      output += "\"}";
+      file = root.openNextFile();
+    }
   }
   output += "]";
   server.send(200, "text/json", output);
